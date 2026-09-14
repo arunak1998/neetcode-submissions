c680d9ddfe231a737dca28234db99f0b9ba7f0e2
Arunkumar Ravichandran <162681594+arunak1998@users.noreply.github.com>
2026-09-14 18:04:32 +0530

    Add: merge-triplets-to-form-target - submission-1

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+
+        good=set()
+
+        for trip in triplets:
+            if trip[0]>target[0] or trip[1]>target[1] or trip[2]>target[2]:
+                continue
+
+            for i,val in enumerate(trip):
+
+                if val==target[i]:
+                    good.add(i)
+
+        return len(good)==3
+        
